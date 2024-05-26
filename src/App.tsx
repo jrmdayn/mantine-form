77ffa342bf4b85d737dbee4bf5901983b2bf6293
Jeremie Dayan <dayanjeremie@gmail.com>
2024-05-26 13:57:52 +0200

    mantine form example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import Form from "./Form";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <div>
+        <Form />
+      </div>
+    </MantineProvider>
+  );
 }
