9c0dd24bf25c411149614c8bb504cbd3655bd2bb
Jeremie Dayan <dayanjeremie@gmail.com>
2024-05-26 15:01:54 +0200

    improvements example

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,35 +1,106 @@
-import { Button, Checkbox, Group, TextInput } from "@mantine/core";
+import { Schema } from "@effect/schema";
+import { Button, Chip, Fieldset, Group, Textarea } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { Array } from "effect";
+
+const improvements = Schema.Literal(
+  "communication",
+  "motivation",
+  "siteFacility",
+  "techIssues",
+  "siteStaff",
+  "badSMO",
+  "quality",
+  "screeningOrEnrollment",
+  "diversity",
+  "budgetOrContracting"
+);
+
+const mapping: Record<typeof improvements.Type, { label: string }> = {
+  communication: {
+    label: "Communication",
+  },
+  motivation: {
+    label: "Motivation",
+  },
+  siteFacility: {
+    label: "Site facility",
+  },
+  techIssues: {
+    label: "Tech issues",
+  },
+  siteStaff: {
+    label: "Site staff",
+  },
+  badSMO: {
+    label: "SMO/Outsourced provider not good",
+  },
+  quality: {
+    label: "Quality concerns",
+  },
+  screeningOrEnrollment: {
+    label: "Screening or enrollment",
+  },
+  diversity: {
+    label: "Diversity",
+  },
+  budgetOrContracting: {
+    label: "Budget & contracting",
+  },
+};
+
+const getLabel = (key: typeof improvements.Type): string => mapping[key].label;
 
 function Form() {
   const form = useForm({
-    mode: "uncontrolled",
+    mode: "controlled",
     initialValues: {
-      email: "",
-      termsOfService: false,
-    },
-
-    validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      improvements: {
+        keys: Array.makeBy(improvements.literals.length, () => false),
+        values: Array.makeBy(improvements.literals.length, () => ""),
+      },
     },
   });
 
   return (
     <form onSubmit={form.onSubmit((values) => console.log(values))}>
-      <TextInput
-        withAsterisk
-        label="Email"
-        placeholder="your@email.com"
-        key={form.key("email")}
-        {...form.getInputProps("email")}
-      />
-
-      <Checkbox
-        mt="md"
-        label="I agree to sell my privacy"
-        key={form.key("termsOfService")}
-        {...form.getInputProps("termsOfService", { type: "checkbox" })}
-      />
+      <Fieldset
+        variant="unstyled"
+        styles={{ root: { display: "flex", gap: "8px", flexWrap: "wrap" } }}
+      >
+        {improvements.literals.map((key, idx) => (
+          <Chip
+            key={form.key(`improvements.keys.${idx}`)}
+            {...form.getInputProps(`improvements.keys.${idx}`)}
+          >
+            {getLabel(key)}
+          </Chip>
+        ))}
+      </Fieldset>
+      <Fieldset
+        variant="unstyled"
+        styles={{
+          root: {
+            display: "flex",
+            flexDirection: "column",
+            gap: "8px",
+            marginTop: "24px",
+          },
+        }}
+      >
+        {form
+          .getValues()
+          .improvements.keys.map(
+            (selected, idx) =>
+              selected && (
+                <Textarea
+                  key={form.key(`improvements.values.${idx}`)}
+                  {...form.getInputProps(`improvements.values.${idx}`)}
+                  label={getLabel(improvements.literals[idx])}
+                />
+              )
+          )}
+      </Fieldset>
 
       <Group justify="flex-end" mt="md">
         <Button type="submit">Submit</Button>
